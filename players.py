015184b69fcafa1b66eaee9cc1df88572a8e4339
SirboksOSU <urrutia.ap@obmontessori.edu.ph>
2026-03-02 15:45:28 +0800

    Add files via upload

---
+++ players.py
@@ -0,0 +1,12 @@
+players = [
+    "Juan Dela Cruz",
+    "Maria Santos",
+    "Carlo Reyes",
+    "Anne Cruz",
+    "Joshua Lim"
+]
+
+print("<ul>")
+for player in players:
+    print("<li>" + player + "</li>")
+print("</ul>")
